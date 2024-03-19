091fdd4b128152a9399ba88bf55d0ec2be0f4b10
Savely <olegych@DESKTOP-6FJCQEC.localdomain>
2024-03-19 11:01:04 +0400

    set publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/PostsList/" : "/",
   transpileDependencies: true,
   lintOnSave: false,
   css: {
